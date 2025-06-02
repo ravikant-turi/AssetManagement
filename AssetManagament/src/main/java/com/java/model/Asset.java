8c91e021fec995c7d6f374a5b3e34e91b8576511
Ravikant Turi <ravikantt@infinite.com>
2025-06-02 13:24:11 +0530

    message

---
+++ AssetManagament/src/main/java/com/java/model/Asset.java
@@ -0,0 +1,41 @@
+package com.java.model;
+
+
+
+public class Asset {
+	
+	private int assetid;
+	
+	private String name;
+	
+	private String quantity;
+
+	public int getId() {
+		return assetid;
+	}
+
+	public void setId(int id) {
+		this.assetid = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+	
+	
+	
+
+}
